c582c0ecbd31150bb494ab1f7523ca8d92f89882
Esther <githubesther@gmail.com>
2025-12-09 11:03:38 +0100

    Feat: adding jakarta validations

--- src/main/java/cat/itacademy/s04/s02/n01/fruit/controllers/FruitController.java
+++ src/main/java/cat/itacademy/s04/s02/n01/fruit/controllers/FruitController.java
@@ -1,8 +1,25 @@
 package cat.itacademy.s04.s02.n01.fruit.controllers;
 
-public class FruitController {
+import cat.itacademy.s04.s02.n01.fruit.model.Fruit;
+import cat.itacademy.s04.s02.n01.fruit.services.FruitService;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-    //TODO: POST	/fruits	Crear fruita
+public class FruitController {
+    private final FruitService fruitService;
+
+    public FruitController(FruitService fruitService) {
+        this.fruitService = fruitService;
+    }
+
+//TODO: POST	/fruits	Crear fruita
+
+    @PostMapping("/fruits")
+    public Fruit postFruit (@RequestBody String name, int kilos){
+
+        return fruitService.save(fruitRequest);
+    }
     //TODO: PUT	/fruits/{id}	Actualitzar fruita
     //TODO: DELETE	/fruits/{id}	Eliminar per id
     //TODO: GET	/fruits/{id}	Obtenir una fruita per id
--- src/main/java/cat/itacademy/s04/s02/n01/fruit/model/Fruit.java
+++ src/main/java/cat/itacademy/s04/s02/n01/fruit/model/Fruit.java
@@ -3,13 +3,18 @@ package cat.itacademy.s04.s02.n01.fruit.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Fruit {
     @Id
     @GeneratedValue
     private Long id;
+    @NotBlank(message = "Name is required")
     private String name;
+    @NotBlank(message = "Kilos are required")
+    @Min(value = 1, message = "Kilos must be at least 1")
     private int weightInKilos;
 
     public Fruit(String name, int weightInKilos) {
