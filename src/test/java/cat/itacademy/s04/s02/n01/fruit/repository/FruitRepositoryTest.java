2762f5e7dae5de761a54e363bf14e9ddb81b3b69
Esther <githubEsther@gmail.com>
2025-12-14 21:38:44 +0100

    Feat: adding test in FruitRepositoryTest.java

--- src/test/java/cat/itacademy/s04/s02/n01/fruit/repository/FruitRepositoryTest.java
+++ src/test/java/cat/itacademy/s04/s02/n01/fruit/repository/FruitRepositoryTest.java
@@ -1,6 +1,7 @@
 package cat.itacademy.s04.s02.n01.fruit.repository;
 
 import cat.itacademy.s04.s02.n01.fruit.fruit.dto.Fruit;
+import cat.itacademy.s04.s02.n01.fruit.fruit.exception.InvalidFruitRequestException;
 import cat.itacademy.s04.s02.n01.fruit.fruit.repository.FruitRepository;
 import cat.itacademy.s04.s02.n01.fruit.provider.dto.Provider;
 import org.junit.jupiter.api.Assertions;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 class FruitRepositoryTest {
@@ -31,6 +33,21 @@ class FruitRepositoryTest {
         assertDoesNotThrow(() -> fruitRepository.save(fruit));
         Assertions.assertEquals("Poma", savedFruit.getName());
         Assertions.assertEquals(1, savedFruit.getWeightInKilos());
+    }
+
+    @Test()
+    @DisplayName("Test must return error if the name is empty")
+    void save_testMustPasAndReturnErrorIfTheNameIsEmpty() {
+        Provider.ProviderRequest providerRequest = new Provider.ProviderRequest();
+        providerRequest.setName("Las Frutas");
+        providerRequest.setCountry("Spain");
+        Provider provider =new Provider(providerRequest);
+
+        Fruit fruit = new Fruit(null, 1, provider);
+        Fruit savedFruit = fruitRepository.save(fruit);
+
+        Assertions.assertThrows(InvalidFruitRequestException.class, () -> fruitRepository.save(fruit));
+
     }
 
 }
