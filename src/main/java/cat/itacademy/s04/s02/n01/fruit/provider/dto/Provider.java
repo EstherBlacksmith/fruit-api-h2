3bf1152475bf2f7d24b346b1568b04e55e8f7361
Esther <githubEsther@gmail.com>
2025-12-14 20:57:35 +0100

    Feat: adding H2 configuration

--- src/main/java/cat/itacademy/s04/s02/n01/fruit/provider/dto/Provider.java
+++ src/main/java/cat/itacademy/s04/s02/n01/fruit/provider/dto/Provider.java
@@ -12,7 +12,7 @@ import lombok.Setter;
 })
 public class Provider {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Setter
     @NotBlank(message = "Name is required")
